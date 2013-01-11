607a8d375ba42db77b63f124e8289e6e7d4711dc
nwillia2 <nwillia2@glam.ac.uk>
2013-01-11 15:00:13 +0000

    Trying to get gem working

--- lib/breadrole/engine.rb
+++ lib/breadrole/engine.rb
@@ -1,11 +1,13 @@
 module Breadrole
   class Engine < Rails::Engine
 
-    config.after_initialize do |app|
+    initializer "breadrole.load_app_instance_data" do |app|
       Breadrole.setup do |config|
         config.app_root = app.root
       end
-      
+    end
+
+    initializer "breadrole.load_static_assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
 
