d67d579ee0d93cfbf96c6ca379c109e76d9fea29
nwillia2 <nwillia2@glam.ac.uk>
2013-01-11 15:17:56 +0000

    Trying to get gem working

--- lib/breadrole/engine.rb
+++ lib/breadrole/engine.rb
@@ -1,6 +1,8 @@
 module Breadrole
   class Engine < Rails::Engine
 
+    config.autoload_paths << File.expand_path("../lib/breadrole/", __FILE__)
+
     initializer "breadrole.load_app_instance_data" do |app|
       Breadrole.setup do |config|
         config.app_root = app.root
